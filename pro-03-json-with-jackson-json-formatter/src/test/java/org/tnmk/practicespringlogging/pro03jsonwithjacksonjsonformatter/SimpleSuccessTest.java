63fb686b7ff7676398e8c3d2bab73eeb2804407d
trank <Kevin.Tran@pointclickcare.com>
2021-06-05 16:12:40 -0400

    Fix SimpleSuccessTest.java in pro-03 after upgrade spring boot version to 2.5.0

--- pro-03-json-with-jackson-json-formatter/src/test/java/org/tnmk/practicespringlogging/pro03jsonwithjacksonjsonformatter/SimpleSuccessTest.java
+++ pro-03-json-with-jackson-json-formatter/src/test/java/org/tnmk/practicespringlogging/pro03jsonwithjacksonjsonformatter/SimpleSuccessTest.java
@@ -1,12 +1,9 @@
 package org.tnmk.practicespringlogging.pro03jsonwithjacksonjsonformatter;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.tnmk.practicespringlogging.pro03jsonwithjacksonjsonformatter.story.SimpleLogWriting;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SimpleSuccessTest {
 
