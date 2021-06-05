1e8d91ed2adeb47fb36379cac724e1477e7ef8a1
trank <Kevin.Tran@pointclickcare.com>
2021-06-05 16:03:22 -0400

    Remove groovy & spock test files

--- pro-03-json-with-jackson-json-formatter/src/test/java/org/tnmk/practicespringlogging/pro03jsonwithjacksonjsonformatter/SimpleSuccessTest.java
+++ pro-03-json-with-jackson-json-formatter/src/test/java/org/tnmk/practicespringlogging/pro03jsonwithjacksonjsonformatter/SimpleSuccessTest.java
@@ -3,20 +3,14 @@ package org.tnmk.practicespringlogging.pro03jsonwithjacksonjsonformatter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.tnmk.practicespringlogging.pro03jsonwithjacksonjsonformatter.Pro03JsonWithJacksonJsonFormatterApplication;
 import org.tnmk.practicespringlogging.pro03jsonwithjacksonjsonformatter.story.SimpleLogWriting;
 
-//Note: the profile is "componenttest", hence the corresonding Spring configuration is "application-componenttest.yml".
-// However, the logback file in test is "logback-test.xml", which has nothing to do with the Spring profile name.
-@ActiveProfiles("componenttest")
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Pro03JsonWithJacksonJsonFormatterApplication.class)
+@SpringBootTest
 public class SimpleSuccessTest {
 
     private SimpleLogWriting simpleLogWriting = new SimpleLogWriting();
-
 
     @Test
     public void testCanLoadListWithOneItem() {
