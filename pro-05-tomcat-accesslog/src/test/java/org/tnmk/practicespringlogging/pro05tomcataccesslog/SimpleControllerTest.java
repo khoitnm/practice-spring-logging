0a1a339591b3fe601a7d3d8ba7fab6f5cbcbab0c
trank <Kevin.Tran@pointclickcare.com>
2023-01-09 10:54:24 -0500

    pro-05-tomcat-accesslog: Remove all redundant dependencies

--- pro-05-tomcat-accesslog/src/test/java/org/tnmk/practicespringlogging/pro05tomcataccesslog/SimpleControllerTest.java
+++ pro-05-tomcat-accesslog/src/test/java/org/tnmk/practicespringlogging/pro05tomcataccesslog/SimpleControllerTest.java
@@ -1,10 +1,8 @@
 package org.tnmk.practicespringlogging.pro05tomcataccesslog;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.tnmk.practicespringlogging.pro05tomcataccesslog.story.SimpleController;
 
 //Note: the profile is "componenttest", hence the corresonding Spring configuration is "application-componenttest.yml".
@@ -12,7 +10,6 @@ import org.tnmk.practicespringlogging.pro05tomcataccesslog.story.SimpleControlle
 // It means that when running this test, it will write log with configuration in "logback-test.xml" in the `test` folder,
 // not with "logback-spring.xml" in the `main` folder.
 @ActiveProfiles("componenttest")
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Pro05TomcatAccesslogApplication.class)
 public class SimpleControllerTest {
 
