b473388fcb02e9210bad4f3205dd47100152c88b
trank <Kevin.Tran@pointclickcare.com>
2023-01-10 16:25:49 -0500

    Log http request.

--- pro-06-tomcat-accesslog-with-security/src/main/java/org/tnmk/practicespringlogging/pro06tomcataccesslogwithsecurity/config/TracingFilter.java
+++ pro-06-tomcat-accesslog-with-security/src/main/java/org/tnmk/practicespringlogging/pro06tomcataccesslogwithsecurity/config/TracingFilter.java
@@ -14,33 +14,50 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class TracingFilter extends GenericFilterBean {
-    private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private static final String HEADER_CORRELATION_ID = "CorrelationId";
     private static final String MDC_CORRELATION_ID = "CorrelationId";
+
     @Override
     public void doFilter(
         ServletRequest servletRequest, ServletResponse servletResponse,
         FilterChain filterChain
     ) throws IOException, ServletException {
+        if (!(servletRequest instanceof HttpServletRequest)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         try {
-            String correlationId;
-            if (servletRequest instanceof HttpServletRequest) {
-                HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-                correlationId =
-                    httpServletRequest.getHeader(HEADER_CORRELATION_ID);
-                if (!StringUtils.hasText(correlationId)) {
-                    correlationId = generateCorrelationId();
-                }
-            } else {
-                correlationId = generateCorrelationId();
-            }
-            MDC.put(MDC_CORRELATION_ID, correlationId);
+            String pccCorrelationId = getOrGenerateCorrelationId(httpServletRequest);
+            MDC.put(MDC_CORRELATION_ID, pccCorrelationId);
+
+
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
+            Integer httpStatus = null;
+            if (servletResponse instanceof HttpServletResponse) {
+                HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+                httpStatus = httpServletResponse.getStatus();
+            }
+            log.info("HttpRequest: {} {}, status: {}",
+                httpServletRequest.getMethod(),
+                httpServletRequest.getRequestURL().toString(),
+                httpStatus);
             MDC.remove(MDC_CORRELATION_ID);
         }
+    }
+
+    private String getOrGenerateCorrelationId(HttpServletRequest httpServletRequest) {
+        String correlationId = httpServletRequest.getHeader(HEADER_CORRELATION_ID);
+        if (!StringUtils.hasText(correlationId)) {
+            correlationId = generateCorrelationId();
+        }
+        return correlationId;
     }
 
     private String generateCorrelationId() {
